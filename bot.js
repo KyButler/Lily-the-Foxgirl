ec48d432821841fda568928a7e977aa23c150252
KyButler <kyran@butlerhq.com>
2020-05-19 18:11:18 -0600

    changed the code to say the bots username

--- bot.js
+++ bot.js
@@ -15,19 +15,13 @@ client.on('message', message => {
     if (!message.content.toLowerCase().startsWith(process.env.PREFIX + " ") || message.author.bot) return; // if the message doesn't start with !lily, or is from a bot, return
     const args = message.content.slice(process.env.PREFIX.length + 1).split(/ +/); // regex get rid of extra spaces, allows for input like 
     const command = args.shift().toLowerCase();
-    if (command == "hello"){ 
-        return message.channel.send("Hi, " + message.author.username + "! I'm Billy!");
+    if (command == "hello" || command == "hi"){ 
+        return message.channel.send("Hi, " + message.author.username + "! I'm " + client.user.username + "!");
     }
 
     if (command == "avatar"){
         return message.reply(`${message.author.displayAvatarURL({ format: "png", dynamic: false })}`);
     }
-
-    // if (command == "test"){
-
-    //     return message.channel.send('test worked!');
-    // }
-
 });
 
     client.on('guildMemberAdd', (guildMember) => {
